3f855bc77042152e952321a93202948a68b631bd
Павел <pirogov-p@list.ru>
2022-12-24 12:18:18 +0300

    Merge remote-tracking branch 'origin/master'

--- tinkof/main.py
+++ tinkof/main.py
@@ -14,13 +14,26 @@ try:
     print('#' * 20)
 
     try:
-        with connection:
-            with connection.cursor() as cursor:
-                sql = "SELECT `id_transaction` FROM `tinkoff.transaction` " \
-                      "WHERE `id_transaction` = %s"
-                cursor.execute(sql, ('1'))
-                result = cursor.fetchone()
-                print(result)
+         with connection.cursor() as cursor:
+             my_list = []
+
+             for i in range(4):  # равносильно инструкции for i in 0, 1, 2, 3:
+                 my_list.append("INSERT INTO transaction (id_transation, price_paper, peper_count,transaction_type,paper_id)" \
+                            " VALUES ("i+5", "i*100", "i",'buy',1);"
+
+
+
+             insert_query = "INSERT INTO transaction (id_transation, price_paper, peper_count,transaction_type,paper_id)" \
+                            " VALUES ('Anna', 'qwerty', 'anna@gmail.com');"
+             cursor.execute(insert_query)
+             connection.commit()
+
+        '''with connection.cursor() as cursor:
+            sql = "SELECT `id_transaction` FROM `tinkoff.transaction` " \
+                 "WHERE `id_transaction` = %s"
+            cursor.execute(sql, ('1'))
+            result = cursor.fetchone()
+            print(result)'''
     finally:
         connection.close()
 except Exception as ex:
