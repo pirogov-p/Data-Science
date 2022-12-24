f9c272cfb4413942159772426a826850682eaaf1
Павел <pirogov-p@list.ru>
2022-12-24 14:58:10 +0300

    Merge remote-tracking branch 'origin/master'

--- tinkof/main.py
+++ tinkof/main.py
@@ -13,6 +13,12 @@ try:
     print('CONNECT!')
     print('#' * 20)
     try:
+        #delete data
+        with connection.cursor() as cursor:
+            delete_query = "DELETE FROM transaction WHERE id_transaction >= 5 AND id_transaction < 10;"
+            cursor.execute(delete_query)
+            connection.commit()
+        #insert data series
         with connection.cursor() as cursor:
             insert_list = []
             for i in range(4):  # равносильно инструкции for i in 0, 1, 2, 3:
@@ -20,8 +26,8 @@ try:
                     "INSERT INTO tinkoff.transaction (id_transaction, price_peper, peper_count,transaction_type,paper_id)"
                     " VALUES (" + str(i + 5) + ", " + str(i * 108 + 100) + "," + str(10 - i) + ",'buy',1);")
                 print(insert_list[i])
-                cursor.execute(insert_list[i])
-        connection.commit()
+                cursor.execute(insert_list[i]) 
+        connection.commit() '''
         with connection.cursor() as cursor:
             sql = "SELECT * FROM tinkoff.transaction "
             cursor.execute(sql)
